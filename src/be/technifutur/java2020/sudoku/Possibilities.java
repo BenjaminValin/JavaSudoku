61f5350108cc04c37e7968f08af077b8e3cc1e82
Benjamin Valin <chbench1502@gmail.com>
2020-10-22 16:35:22 +0200

    Corrections et commentaires

--- src/be/technifutur/java2020/sudoku/Possibilities.java
+++ src/be/technifutur/java2020/sudoku/Possibilities.java
@@ -13,8 +13,8 @@ public class Possibilities {
     public boolean add(int i){                                                  //Ajoute une possibilité
         boolean ok = false;
         System.out.println("Ajout de " + i);
-        if (this.contains(i)){
-            this.data = this.data | (1<<(i-1));
+        if (!this.contains(i)){
+            this.data = this.data | (1<<(i-1));                                 //this.data | = (1<<(i-1)) fonctionne aussi
             ok = true;
         }
         System.out.println(Integer.toBinaryString(this.data));
@@ -24,8 +24,8 @@ public class Possibilities {
     public boolean remove(int i){                                               //Retire une possibilité
         boolean ok = false;
         System.out.println("Retrait de " + i);
-        if (!this.contains(i)){
-            this.data = (this.data  ^ (1<<(i-1)));
+        if (this.contains(i)){
+            this.data = (this.data  ^ (1<<(i-1)));                              //this.data = (this.data & ~(1<<(i-1))) fonctionne aussi
             ok = true;
         }
         System.out.println(Integer.toBinaryString(this.data));
@@ -33,12 +33,12 @@ public class Possibilities {
     }
 
     public boolean contains(int i){                                             //Vérifie si un nombre est possible
-        boolean ok = (this.data & (1 << (i - 1))) == 0;
+        boolean ok = (this.data & (1 << (i - 1))) != 0;
         return ok;
     }
 
     public int size(){                                                          //Compte le nombre de possibilités restantes
-        return 9-Integer.bitCount(this.data);
+        return Integer.bitCount(this.data);
     }
 
     public static void main(String[] args) {
@@ -53,15 +53,15 @@ public class Possibilities {
         System.out.println(p.add(9));
         System.out.println(p.add(1));
         System.out.println("Possibilités restantes pour cette case : " + p.size());
-        System.out.println("Puis-je mettre le chiffre 1? " + p.contains(1));
+        System.out.println("Le chiffre 1 est-il déjà placé? : " + p.contains(1));
         System.out.println(p.remove(1));
         System.out.println(p.remove(5));
         System.out.println("Possibilités restantes pour cette case : " + p.size());
         System.out.println(p.remove(8));
         System.out.println(p.remove(2));
-        System.out.println("Puis-je mettre le chiffre 6? " + p.contains(6));
-        System.out.println("Puis-je mettre le chiffre 2? " + p.contains(2));
-        System.out.println("Possibilités restantes pour cette case : " + p.size());
+        System.out.println("Le chiffre 6 est-il déjà placé? : " + p.contains(6));
+        System.out.println("Le chiffre 2 est-il déjà placé? : " + p.contains(2));
+        System.out.println("Possibilités déjà utilisées pour cette case : " + p.size());
 
     }
 }
