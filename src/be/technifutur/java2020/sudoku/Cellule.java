151c4fc997014e04eb919bc1be8229d0bf7a5a15
Benjamin Valin <chbench1502@gmail.com>
2020-10-23 12:30:04 +0200

    Création classe cellule

---
+++ src/be/technifutur/java2020/sudoku/Cellule.java
@@ -0,0 +1,43 @@
+package be.technifutur.java2020.sudoku;
+
+public class Cellule {
+
+    private Possibilities line;
+    private Possibilities column;
+    private Possibilities square;
+    private char valeur;
+    private boolean modif;
+
+    public void addLine(Possibilities p){
+
+    }
+
+    public void addColumn(Possibilities p){
+
+    }
+
+    public void addSquare(Possibilities p){
+
+    }
+
+    public void remove(Possibilities p){
+
+    }
+
+    public void isValid(){
+
+    }
+
+    public void isEmpty(){
+
+    }
+
+    public void setValue(char c){
+        this.valeur = c;
+    }
+
+    public char getValue(){
+        return this.valeur;
+    }
+
+}
